9ec70e22b1639d9b5004f5b57c8c844d8a9ca248
Jerome IZARD <jerome.izard@econocom.com>
2017-11-03 11:53:02 +0100

    Chapter 3 : 5

--- src/app/cocktails-list/cocktails-list.component.ts
+++ src/app/cocktails-list/cocktails-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cocktail } from '../cocktail.model';
 
 @Component({
   selector: 'app-cocktails-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cocktails-list.component.css']
 })
 export class CocktailsListComponent implements OnInit {
+  public cocktails: Cocktail[] = [
+    new Cocktail('Mojito'),
+    new Cocktail('Margarita'),
+    new Cocktail('Sour')
+  ];
 
   constructor() { }
 
