2e055275ef3361c6677f51d13e7390b8b5640b69
Jerome IZARD <jerome.izard@econocom.com>
2017-11-06 16:35:52 +0100

    Chapter 9 : 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { Cocktail } from './shared/cocktail.model';
+import { Cocktail } from './shared/models/cocktail.model';
 
 @Component({
   selector: 'app-root',
--- src/app/cocktail-container/cocktail-details/cocktail-details.component.ts
+++ src/app/cocktail-container/cocktail-details/cocktail-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 
-import { Cocktail } from '../../shared/cocktail.model';
+import { Cocktail } from '../../shared/models/cocktail.model';
 
 @Component({
   selector: 'app-cocktail-details',
