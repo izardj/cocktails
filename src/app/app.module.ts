c21bf3270c6a178385e83b722d19261be8e12c14
Jerome IZARD <jerome.izard@econocom.com>
2017-11-07 09:43:43 +0100

    Chapter 11 : 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CocktailsListComponent } from './cocktail-container/cocktails-list/cocktails-list.component';
-import { CocktailDetailsComponent } from './cocktail-container/cocktail-details/cocktail-details.component';
 import { HeaderComponent } from './header/header.component';
 import { CocktailContainerComponent } from './cocktail-container/cocktail-container.component';
-import { ActiveDirective } from './shared/directives/active.directive';
+import { CocktailsListComponent } from './cocktail-container/cocktails-list/cocktails-list.component';
+import { CocktailDetailsComponent } from './cocktail-container/cocktail-details/cocktail-details.component';
 import { PanierComponent } from './panier/panier.component';
 import { IngredientsListComponent } from './panier/ingredients-list/ingredients-list.component';
+import { ActiveDirective } from './shared/directives/active.directive';
+import { AppRouting } from './app.routing';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { IngredientsListComponent } from './panier/ingredients-list/ingredients-
     IngredientsListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
