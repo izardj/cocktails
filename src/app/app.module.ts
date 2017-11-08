f2026ca77ee51854beb4b3a7f793de26fa9c27ef
Jerome IZARD <jerome.izard@econocom.com>
2017-11-08 16:56:39 +0100

    Chapter 16 : 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -32,6 +33,7 @@ import { FilterPipe } from './shared/pipes/filter.pipe';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
     AppRouting
   ],
   providers: [
